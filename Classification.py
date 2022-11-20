a1ace903a24a6ab01b498aa76b568138548b101c
Oscar Leong <72083307+OscarL63@users.noreply.github.com>
2022-11-20 14:23:27 +0000

    Cleaned up

--- Classification.py
+++ Classification.py
@@ -55,13 +55,9 @@ class Layer():
         self.node_num = node_num
         self.activation_func = activation_func
 
-        #print(node_num)
-
         for node in range(node_num):
             self.nodes.append(Node())
-        
-        #print(self.nodes)
-    
+            
     def get_node(self, node_ind):
         return self.nodes[node_ind]
 
@@ -155,7 +151,6 @@ class Net():
                     prev_node_x_out = prev_layer.get_node(prev_node_ind).get_x_out()
 
                     current_node_v = current_node_v + prev_weight * prev_node_x_out
-                #print(current_node_v)
 
                 # Add bias 
                 current_node_v = current_node_v + node.get_bias()
@@ -183,26 +178,18 @@ class Net():
         current_layer = layer - 1 # s
         current_node = self.network[current_layer].get_node(j)
         current_node_v = current_node.get_v()
-        #print(current_node)
 
         delta_layer = self.network[layer]
 
         delta_sum = 0
-        #print("a")
-        #print("delta layer ", layer)
         
         for delta_ind in range(len(delta_layer.get_nodes())):
             delta_node = delta_layer.get_node(delta_ind)
-            #print("index", len(delta_layer.get_nodes()))
 
             weight = delta_node.get_weight(j)
-            delta_j = delta_node.get_delta()    
-            #print("weight", weight)
-            #print("delta_j", delta_j)
-        
+            delta_j = delta_node.get_delta()     
 
             delta_sum = delta_sum + weight * delta_j
-            #print("delta_prog", delta_sum)
 
         
         cur_activation_func = self.activation_func_list[current_layer]
@@ -211,7 +198,6 @@ class Net():
         delta = delta_sum * self.diff_activation_function(current_node_v, cur_activation_func)
         current_node.set_delta(delta) 
         
-        #print(delta)
 
     def backward_pass(self, desired_array, learning_rate):
 
@@ -241,14 +227,9 @@ class Net():
 
                     weight = node.get_weight(weight_ind)
                     update_weight = weight + learning_rate * node.get_delta() * prev_layer_x_out
-                    #print("x", prev_layer_x_out)
-                    #print("del", node.get_delta())
-                    #print("node ", layer_ind_back, node_ind)
-                    #print("up", update_weight)
-                    #print("ind", weight_ind)
+                    
                     node.set_weight(weight_ind, update_weight)
-                    #print("------")
-                    #print(update_weight)
+
 
                 # Update the bias here !!!!!!!!!!!!!!!!!!!!!!!!!
                 old_bias = node.get_bias()
@@ -257,38 +238,20 @@ class Net():
                 
             # Calculate delta with new updated weights once every layer is completed
             # No need to calculate the delta for the input layer
-            #print("layer", layer_ind_back)
 
             # No need to calculate the delta for the input layer
             if layer_ind_back-1 != 0:
                 pre_layer = self.network[layer_ind_back - 1]
                 
                 for pre_layer_node in range(len(pre_layer.get_nodes())):
-                    #print(len(pre_layer))
-                    #print(pre_layer_node)
+
                     self.calculate_hid_delta(layer_ind_back, pre_layer_node)
-
-                #self.calculate_hid_delta(layer_ind_back, node_ind)
-                #print(layer_ind_back - 1)
-                
-            #print("-------------------------------------")
-
-
 
 
 
 
 # main 
 if __name__ == "__main__":
-    """ network_size = [4,5,3,3]
-    activation_function_list = ["start", "tanh", "tanh","lin"] # always match the size of network size
-    net = Net(network_size, activation_function_list)
-    net.populate_weights()
-    net.input_data([-0.2,0.5,-1.3,0.68])
-    net.forward_pass()
-    desired_array = [0,0,1]
-    learning_rate = 0.01
-    net.backward_pass(desired_array, learning_rate) """
 
     # Hyper parameters
     network_size = [4,5,3,3]
@@ -356,10 +319,7 @@ if __name__ == "__main__":
             correct = correct + 1
 
 
-    print(correct)
-
-
-
+    print(correct/testing_amount)
 
 
     file.close()
